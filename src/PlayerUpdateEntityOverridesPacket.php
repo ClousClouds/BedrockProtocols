e52bc8fcfe18b2daf396f8596d7056b57cb7141c
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-03-15 20:28:43 +0700

    ...

--- src/PlayerUpdateEntityOverridesPacket.php
+++ src/PlayerUpdateEntityOverridesPacket.php
@@ -18,47 +18,47 @@ use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
 use pocketmine\network\mcpe\protocol\types\UpdateType;
 
 class PlayerUpdateEntityOverridesPacket extends DataPacket implements ClientboundPacket{
-	public const NETWORK_ID = ProtocolInfo::PLAYER_UPDATE_ENTITY_OVERRIDES_PACKET;
+    public const NETWORK_ID = ProtocolInfo::PLAYER_UPDATE_ENTITY_OVERRIDES_PACKET;
 
-	public int $targetId;
-	public int $propertyIndex;
-	public UpdateType $updateType;
-	public int|float $value;
+    public int $targetId;
+    public int $propertyIndex;
+    public UpdateType $updateType;
+    public int|float $value;
 
-	/**
-	 * @generate-create-func
-	 */
-	public static function create(int $targetId, int $propertyIndex, \pocketmine\network\mcpe\protocol\types\UpdateType $updateType, int|float $value) : self{
-		$result = new self;
-		$result->targetId = $targetId;
-		$result->propertyIndex = $propertyIndex;
-		$result->updateType = $updateType;
-		$result->value = $value;
-		return $result;
-	}
+    /**
+     * @generate-create-func
+     */
+    public static function create(int $targetId, int $propertyIndex, UpdateType $updateType, int|float $value) : self{
+        $result = new self;
+        $result->targetId = $targetId;
+        $result->propertyIndex = $propertyIndex;
+        $result->updateType = $updateType;
+        $result->value = $value;
+        return $result;
+    }
 
-	protected function decodePayload(PacketSerializer $in) : void{
-		$this->targetId = $in->getActorUniqueId();
-		$this->propertyIndex = $in->getVarInt();
-		$this->updateType = UpdateType::from($in->getVarInt());
-		$this->value = match ($this->updateType) {
-			UpdateType::SetIntOverride => $in->getVarInt(),
-			UpdateType::SetFloatOverride => $in->getLFloat(),
-			default => throw new \UnexpectedValueException("Invalid UpdateType"),
-		};
-	}
+    protected function decodePayload(PacketSerializer $in) : void{
+        $this->targetId = $in->getActorUniqueId();
+        $this->propertyIndex = $in->getVarInt();
+        $this->updateType = UpdateType::from($in->getVarInt());
+        $this->value = match ($this->updateType) {
+            UpdateType::SetIntOverride => $in->getVarInt(),
+            UpdateType::SetFloatOverride => $in->getLFloat(),
+            default => throw new \UnexpectedValueException("Invalid UpdateType"),
+        };
+    }
 
-	protected function encodePayload(PacketSerializer $out) : void{
-		$out->putActorUniqueId($this->targetId);
-		$out->putVarInt($this->propertyIndex);
-		$out->putVarInt($this->updateType->value);
-		match ($this->updateType) {
-			UpdateType::SetIntOverride => $out->putVarInt($this->value),
-			UpdateType::SetFloatOverride => $out->putLFloat($this->value),
-		};
-	}
+    protected function encodePayload(PacketSerializer $out) : void{
+        $out->putActorUniqueId($this->targetId);
+        $out->putVarInt($this->propertyIndex);
+        $out->putVarInt($this->updateType->value);
+        match ($this->updateType) {
+            UpdateType::SetIntOverride => $out->putVarInt($this->value),
+            UpdateType::SetFloatOverride => $out->putLFloat($this->value),
+        };
+    }
 
-	public function handle(PacketHandlerInterface $handler) : bool{
-		return $handler->handlePlayerUpdateEntityOverrides($this);
-	}
+    public function handle(PacketHandlerInterface $handler) : bool{
+        return $handler->handlePlayerUpdateEntityOverrides($this);
+    }
 }
