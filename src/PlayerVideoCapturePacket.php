7d141af059c41636c677e105b9365ecb4b52946f
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-03-15 20:27:08 +0700

    Update PlayerVideoCapturePacket.php

--- src/PlayerVideoCapturePacket.php
+++ src/PlayerVideoCapturePacket.php
@@ -17,36 +17,36 @@ namespace pocketmine\network\mcpe\protocol;
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
 
 class PlayerVideoCapturePacket extends DataPacket implements ServerboundPacket{
-	public const NETWORK_ID = ProtocolInfo::PLAYER_VIDEO_CAPTURE_PACKET;
+    public const NETWORK_ID = ProtocolInfo::PLAYER_VIDEO_CAPTURE_PACKET;
 
-	public bool $action;
-	public int $frameRate;
-	public string $filePrefix;
+    public bool $action;
+    public int $frameRate;
+    public string $filePrefix;
 
-	/**
-	 * @generate-create-func
-	 */
-	public static function create(bool $action, int $frameRate, string $filePrefix) : self{
-		$result = new self;
-		$result->action = $action;
-		$result->frameRate = $frameRate;
-		$result->filePrefix = $filePrefix;
-		return $result;
-	}
+    /**
+     * @generate-create-func
+     */
+    public static function create(bool $action, int $frameRate, string $filePrefix) : self{
+        $result = new self;
+        $result->action = $action;
+        $result->frameRate = $frameRate;
+        $result->filePrefix = $filePrefix;
+        return $result;
+    }
 
-	protected function decodePayload(PacketSerializer $in) : void{
-		$this->action = $in->getBool();
-		$this->frameRate = $in->getUnsignedVarInt();
-		$this->filePrefix = $in->getString();
-	}
+    protected function decodePayload(PacketSerializer $in) : void{
+        $this->action = $in->getBool();
+        $this->frameRate = $in->getUnsignedVarInt();
+        $this->filePrefix = $in->getString();
+    }
 
-	protected function encodePayload(PacketSerializer $out) : void{
-		$out->putBool($this->action);
-		$out->putUnsignedVarInt($this->frameRate);
-		$out->putString($this->filePrefix);
-	}
+    protected function encodePayload(PacketSerializer $out) : void{
+        $out->putBool($this->action);
+        $out->putUnsignedVarInt($this->frameRate);
+        $out->putString($this->filePrefix);
+    }
 
-	public function handle(PacketHandlerInterface $handler) : bool{
-		return $handler->handlePlayerVideoCapture($this);
-	}
+    public function handle(PacketHandlerInterface $handler) : bool{
+        return $handler->handlePlayerVideoCapture($this);
+    }
 }
